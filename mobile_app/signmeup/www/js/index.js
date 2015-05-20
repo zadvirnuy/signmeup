c5d8cc58511a51dd97f56ef92f8ad36781516074
Admin <yuriy.zadvirnyy@perfectial.com>
2015-05-20 12:03:42 +0300

    delete old version

--- mobile_app/signmeup/www/js/index.js
+++ mobile_app/signmeup/www/js/index.js
@@ -59,14 +59,14 @@ var app = {
         scanner.scan( function (result) { 
 
             alert("We got a barcode\n" + 
-            "Result: " + result.text + "\n" + 
-            "Format: " + result.format + "\n" + 
-            "Cancelled: " + result.cancelled);  
+            "Result: " + result.text);      // + "\n" + 
+            // "Format: " + result.format + "\n" + 
+            //"Cancelled: " + result.cancelled
 
            console.log("Scanner result: \n" +
-                "text: " + result.text + "\n" +
-                "format: " + result.format + "\n" +
-                "cancelled: " + result.cancelled + "\n");
+                "text: " + result.text); //+ "\n" +
+                //"format: " + result.format + "\n" +
+                //"cancelled: " + result.cancelled + "\n"
             document.getElementById("info").innerHTML = result.text;
             console.log(result);
             /*
